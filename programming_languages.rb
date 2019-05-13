f14a38722fb67f388ccfb02ad67624f20cea56e4
Joe Jamsky <cba1067950@yahoo.com>
2019-05-13 20:45:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,6 +5,7 @@ def reformat_languages(languages)
   
   languages.each do |lvl1, lvl1_data|   #oo/func, lang >
     lvl1_data.each do |lvl2, lvl2_data| #lang, type >
+<<<<<<< HEAD
       if new_hash.has_key?(lvl2) == false
         new_hash[lvl2] = lvl2_data
       end
@@ -14,11 +15,20 @@ def reformat_languages(languages)
       new_hash[lvl2][:style] << lvl1
     end
   end
+=======
+      new_hash[lvl2] = lvl2_data
+      binding.pry
+    end
+              
+  end
+
+>>>>>>> 7bb58516ce4d905d8658a1a1b96ccd97e1b50e40
   new_hash
 end
 
 language_hash = {
   :oo => {
+<<<<<<< HEAD
   #  :ruby => {
   #    :type => "interpreted"
   #  },
@@ -42,6 +52,31 @@ language_hash = {
   #  :scala => {
   #    :type => "compiled"
   #  },
+=======
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+>>>>>>> 7bb58516ce4d905d8658a1a1b96ccd97e1b50e40
     :javascript => {
       :type => "interpreted"
     }
